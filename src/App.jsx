b9826dfdc218b0875e78572cac8e37c0f7b40754
komaldeshmukh999 <komaldeshmukh64670@gmail.com>
2025-08-04 09:45:55 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import './App.css'
 import Footer from './components/Footer/Footer'
 import Nav from './components/Nav'
-import Home from './pages/home'
+import Home from './pages/Home'
 
 function App() {
 
